64f4c297e38d46a57f02f1fbae580a958ee94abd
Soham Samanta <edittlordd@gmail.com>
2026-07-19 21:57:40 +0530

    Update +page.svelte

--- src/routes/stats/+page.svelte
+++ src/routes/stats/+page.svelte
@@ -3,6 +3,6 @@
 
 <div class="px-1 flex flex-col items-stretch gap-8">
     <h2 class="pt-4 font-mono font-semibold text-2xl text-center">GitHub Stats</h2>
-    <img alt="github-stats" class="h-48" src="https://github-readme-stats-zapd0s.vercel.app/api?username=sxham69&show_icons=true&theme=transparent&text_color=00dfd8&title_color=00dfd8&text_bold=false&include_all_commits=true">
-    <img alt="github-stats" class="h-48" src="https://github-readme-stats-zapd0s.vercel.app/api/top-langs/?username=sxham69&show_icons=true&theme=transparent&layout=donut&text_color=00dfd8&title_color=00dfd8">
+    <img alt="github-stats" class="h-48" src="https://github-readme-stats.shion.dev/api?username=sxham69&theme=dark&hide_border=false&include_all_commits=false&count_private=false">
+    <img alt="github-stats" class="h-48" src="https://github-readme-stats.shion.dev/api/top-langs/?username=sxham69&theme=dark&hide_border=false&include_all_commits=false&count_private=false&layout=compact">
 </div>
